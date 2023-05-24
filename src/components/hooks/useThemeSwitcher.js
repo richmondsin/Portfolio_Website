d68c2b2541d29fdbc695c03a37111ed7c557dfe9
Richmond Sin <richmondsin.rs@gmail.com>
2023-05-25 01:37:22 +0800

    initial

--- src/components/hooks/useThemeSwitcher.js
+++ src/components/hooks/useThemeSwitcher.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
+simport React, { useEffect, useState } from 'react'
 
 const useThemeSwitcher = () => {
 
-    const preferDarkQuery = "(prefer-color-scheme: dark)"
+    const preferDarkQuery = "(prefers-color-scheme: dark)"
     const [mode, setMode] = useState("")
     
     useEffect(() => {
