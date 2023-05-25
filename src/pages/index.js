36c03a31a376cc15b4f9ac454f1b291a0ae79ab0
Richmond Sin <richmondsin.rs@gmail.com>
2023-05-25 14:48:30 +0800

    initial

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,9 +58,9 @@ export default function Home() {
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6
                   rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
-                  border-1 border-solid border-transparent hover:border-dark
+                  border-0.5 border-solid border-transparent hover:border-dark
                   dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light
-                  hover:dark:border-light md:p-2 md:px-4 md:text-base sm:border-1 xs:border-1 
+                  hover:dark:border-light md:p-2 md:px-4 md:text-base 
                   "
                   download={true}
                 >
