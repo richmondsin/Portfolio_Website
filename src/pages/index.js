459e554f79cd4110f10c5c967a07dc1c5475f4b8
Richmond Sin <richmondsin.rs@gmail.com>
2023-05-25 17:12:05 +0800

    initial

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,14 +57,15 @@ export default function Home() {
                   href="/Resume.pdf"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6
-                  rounded-lg text-lg font-semibold
-                  border-1 border-solid border-transparent
-                  dark:bg-light dark:text-dark md:p-2 md:px-4 md:text-base sm:p-1.5 sm:px-2 sm:text-sm
+                  rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
+                  border-1 border-solid border-transparent hover:border-dark
+                  dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light
+                  hover:dark:border-light md:p-2 md:px-4 md:text-base sm:p-1.5 sm:px-2 sm:text-sm
                   xs:p-1 xs:px-2 xs:text-xs
                   "
                   download={true}
                 >
-                  Resume <LinkArrow className={"w-6 ml-1"} />
+                  Resume <LinkArrow className={"!w-6 ml-1"} />
                 </Link>
                 <Link
                   href="mailto:richmondsin.rs@gmail.com"
